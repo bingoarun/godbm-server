b3aa5f041fae6a1cb0a88218e1f560816643f81e
Arun prasath <arunprasath33@gmail.com>
2016-09-07 17:00:14 +0530

    Minor commit

--- server.go
+++ server.go
@@ -1,16 +1,33 @@
 
 package main 
 
-import "fmt"
-import "net/http"
-import "io"
+import (
+    "io"
+    "net/http"
+)
 
-func ProcessMessage(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "Hello world !")
-    fmt.Println("Printer")
+func RootCall(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "API server is running\n")
 }
 
+var mux map[string]func(http.ResponseWriter, *http.Request)
+
 func main() {
-    http.HandleFunc("/", ProcessMessage)
-    http.ListenAndServe(":8000", nil)
+    server := http.Server {
+        Addr: ":8000",
+        Handler: &myHandler{},
+    }
+    mux = make(map[string]func(http.ResponseWriter, *http.Request))
+    mux["/"] = RootCall
+    server.ListenAndServe()
+}
+
+type myHandler struct{}
+
+func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+    if h, ok := mux[r.URL.String()]; ok {
+        h(w, r)
+        return
+    }
+    io.WriteString(w, "Unknown URL call: "+r.URL.String())
 }
